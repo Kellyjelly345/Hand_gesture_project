c60024842bdc7404147de397efd2a8549b1c5395
Kellyjelly345 <92412083+Kellyjelly345@users.noreply.github.com>
2022-11-05 16:58:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+//https://teachablemachine.withgoogle.com/models/-66fZGxYz/
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+})
+
+camera = document.getElementById("cameraa");
+Webcam.attach("#camera")
+
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = "<img id='picture' src=" +data_uri+ ">"
+        
+    })
+}
+
+console.log("ml5 version",ml5.version)
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/-66fZGxYz/model.json",modelLoaded)
+function modelLoaded(){
+    console.log("model Loaded")
+}
+
+function speak(){
+    var synth = window.speechSynthesis
+    speak_data1 = "The first prediction is "+ prediction_1
+    speak_data2 = "The second prediction is "+ prediction_2
+    var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2)
+    synth.speak(utterThis)
+}
